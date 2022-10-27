80b16a0814df14fcfcfbc92cb5504fdaabc00c90
IntoFantasy <sky-walker@sjtu.edu.cn>
2022-10-27 21:15:16 +0800

    UI

---
+++ UIPrototype/code/profile/script.js
@@ -0,0 +1,11 @@
+function setBarWidth(dataElement, barElement, cssProperty, barPercent) {
+  var listData = [];
+  $(dataElement).each(function() {
+    listData.push($(this).html());
+  });
+  var listMax = Math.max.apply(Math, listData);
+  $(barElement).each(function(index) {
+    $(this).css(cssProperty, (listData[index] / listMax) * barPercent + "%");
+  });
+}
+setBarWidth(".style-1 span", ".style-1 em", "width", 100);
